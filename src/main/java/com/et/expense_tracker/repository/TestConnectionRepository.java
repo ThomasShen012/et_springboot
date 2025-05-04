844ba2e7b414caa4106edf89e51f76174b101f16
Thomasshen012 <thomasshen012@gmail.com>
2025-05-04 20:17:42 +0800

    delete testing connection

--- src/main/java/com/et/expense_tracker/repository/TestConnectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.et.expense_tracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.et.expense_tracker.model.TestConnection;
-
-public interface TestConnectionRepository extends JpaRepository<TestConnection, Long> {
-    
-}
